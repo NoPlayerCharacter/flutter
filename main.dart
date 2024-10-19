de855992f6bf8eab84b357d312668aecc2fa453e
Mr.T <147992689+NoPlayerCharacter@users.noreply.github.com>
2024-10-19 11:08:54 +0800

    as

--- main.dart
+++ main.dart
@@ -6,7 +6,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget buil(BuildContext context) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: LoginPage(),
     );
