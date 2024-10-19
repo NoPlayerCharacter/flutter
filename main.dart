05d25813f9f4c47d7a12bb4663e1a606db9bfef0
Mr.T <147992689+NoPlayerCharacter@users.noreply.github.com>
2024-10-19 10:57:08 +0800

    hapus assets

--- main.dart
+++ main.dart
@@ -25,7 +25,7 @@ class LoginPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Image.asset(
-                  'assets/UNDIKNAS.jpeg'), // Tambahkan gambar logo Anda di sini
+                  'UNDIKNAS.jpeg'), // Tambahkan gambar logo Anda di sini
               SizedBox(height: 20),
               TextField(
                 decoration: InputDecoration(
